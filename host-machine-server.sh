c89b0aa4b6271c178da6be4f950f90b7c4638be7
marquis-sharefbs <official.sharefmbs@gmail.com>
2026-05-01 11:36:33 -0700

    Updated host-machine-server.sh to reflect progress. Add a clean up feature and make executable for testing.

--- host-machine-server.sh
+++ host-machine-server.sh
@@ -1,8 +1,29 @@
+#!/usr/bin/env bash
+
+'''
+General Idea for this script is the following
+
 # Direct to folder containing web files
 cd ~/Documents/coding/projects/personal-portfolio-website/
 
 # Use Python to create a local server on host machine
 python3 -m http.server 8000
 
-# Open run the server in host machine's browser
+# Open run the server in host machine browser
 http://localhost:8000
+'''
+
+# Configure port and location of server persistence
+PORT=8000
+DIR="$HOME/Documents/coding/projects/personal-portfolio-website"
+
+# Start server in background
+python3 -m http.server "$PORT" & SERVER_PID=$!
+
+echo "Server started on http://localhost:#PORT (PID: $SERVER_PID)"
+
+# Open in host machine browser
+xdg-open "http://localhost:$PORT" >/dev/null 2>&1
+
+# Keep script running
+wait "$SERVER_PID"
